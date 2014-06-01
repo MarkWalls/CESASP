f9df3fcb3f3d9b2b93325feb629c09817a6b574e
Mark Walls <dwalls724@gmail.com>
2014-05-31 21:56:25 -0700

    BaseCommit of ASP structure out of the box

---
+++ CES.ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CES.ASP.Startup))]
+namespace CES.ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
